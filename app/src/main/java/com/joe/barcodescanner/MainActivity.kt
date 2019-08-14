1c826887e020672970ed384ab53447058820777c
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-08-14 16:40:02 +0300

    Show alert dialog when requesting camera permissions

--- app/src/main/java/com/joe/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/joe/barcodescanner/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.joe.barcodescanner
 
 import android.Manifest.permission.CAMERA
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.Result
@@ -32,6 +35,40 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
     private fun requestPermission() {
         ActivityCompat.requestPermissions(this, arrayOf(CAMERA), REQUEST_CAMERA)
+    }
+
+    private fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: Array<Int>) {
+        when (requestCode) {
+            REQUEST_CAMERA -> {
+                if (grantResults.isNotEmpty()) {
+                    val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    if (cameraAccepted) {
+                        Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                displayAlertMessage("Access to camera required",
+                                    DialogInterface.OnClickListener { _, _ ->
+                                        requestPermissions(arrayOf(CAMERA), REQUEST_CAMERA)
+                                    }
+                                )
+                                return
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun displayAlertMessage(message: String, listener: DialogInterface.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", listener)
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
     }
 
     override fun handleResult(result: Result?) {
