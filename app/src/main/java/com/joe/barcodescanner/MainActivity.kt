e77f60e3861ed1e3e2e5a9017f1d16483c783d4e
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-08-14 15:40:44 +0300

    Create a ZXingScannerView object

--- app/src/main/java/com/joe/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/joe/barcodescanner/MainActivity.kt
@@ -2,11 +2,26 @@ package com.joe.barcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+
+    private lateinit var scannerView: ZXingScannerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        scannerView = ZXingScannerView(this)
+        setContentView(scannerView)
     }
+
+    override fun handleResult(result: Result?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        const val REQUEST_CAMERA = 100
+    }
+
 }
