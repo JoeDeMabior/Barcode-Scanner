762865e83f2f2fd02613a972b498c1e77039cff1
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-08-14 15:55:26 +0300

    Request camera permission

--- app/src/main/java/com/joe/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/joe/barcodescanner/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.joe.barcodescanner
 
+import android.Manifest.permission.CAMERA
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
@@ -14,6 +19,19 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
         scannerView = ZXingScannerView(this)
         setContentView(scannerView)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            if (!checkPermission())
+                requestPermission()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(CAMERA), REQUEST_CAMERA)
     }
 
     override fun handleResult(result: Result?) {
